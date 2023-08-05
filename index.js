dcf0dde092ebd6674b920c293fd2e4015dd1d47b
haku-ohsumi <haku818181@gmail.com>
2023-08-05 23:15:08 +0900

    urlによって表示を変えるよ

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require ("http");
 
 http.createServer (function (req, res){
-    console.log("reqの中身: ", req);
-    res.setHeader ("Content-Type", "text/plain;charset=utf-8");
-    res.write ("こんにちは");
-    res.end();
+    if(req.url === "/") {
+      res.setHeader("Content-Type", "text/plain;charset=utf-8");
+      res.write("こんにちは");
+      res.end();
+    } else if(req.url === "/about"){
+      res.setHeader("Content-Type", "text/plain;charset=utf-8");
+      res.write("ここはAboutページです。");
+      res.end();
+    }
   }).listen(4000, function(){
     console.log("Listening on localhost port 4000");
 });
